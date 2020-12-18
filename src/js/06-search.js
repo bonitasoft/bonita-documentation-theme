7c6ed2dbaeb0af87d497d04b51b374034814e0d4
Adrien <adrien.lachambre@bonitasoft.com>
2020-12-18 11:13:42 +0100

    improve mobile view (#7)
    Co-authored-by: VishalThapaliya <vishal.thapaliya@gmail.com>

---
+++ src/js/06-search.js
@@ -0,0 +1,19 @@
+console.log('Hello BIBI')
+
+var searchIcon = document.getElementById('search-icon')
+var searchBar = document.getElementById('search-bar')
+
+searchIcon.onclick = function () {
+  //Focus Search Bar (Expand it).
+  searchBar.focus()
+}
+
+searchBar.onblur = function () {
+  //Empty the input content
+  this.value = ''
+}
+
+searchBar.onkeydown = function (e) {
+  // search behavior -> should be handled by algolia
+  // if (e.key === 'Enter') alert('Searching for: ' + this.value)
+}
