512c8a912003b75bfb072b49afc9877a53a0a1d9
Adrien <adrien.lachambre@bonitasoft.com>
2021-03-22 15:45:25 +0100

    AVEC LE ADD C EST TOUJOURS MIEU

---
+++ src/js/06-docsearch.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+function sendClickEvent (queryID, hits, suggestion) {
+  // The APP ID does not provide any write access, it is used ot identify us when using API's
+  var appID = 'BH4D9OD16A'
+  var apiKey = '16267f96d135c47df8454efd5b448c9a'
+
+  const Http = new XMLHttpRequest()
+  Http.open('POST', 'https://insights.algolia.io/1/events')
+
+  Http.setRequestHeader('x-algolia-api-key', apiKey)
+  Http.setRequestHeader('x-algolia-application-id', appID)
+  Http.setRequestHeader('Content-Type', 'application/json')
+
+  var position
+  var objectID
+
+  for (let i = 0; i < hits.length; i++) {
+    var hit = hits[i]
+    if (hit.url === suggestion.url) {
+      position = i + 1
+      objectID = hit.objectID
+      break
+    }
+  }
+
+  // Seems that Gulp uglify is unable to deal with proper js multiline String...
+  const body = '{' +
+      '"events": [' +
+        '{' +
+          '"eventType": "click",' +
+          '"eventName": "Search suggestion selected",' +
+          '"index": "bonitasoft",' +
+          '"userToken": "bonitadoc",' +
+          '"objectIDs": ["' + objectID + '"],' +
+          '"queryID": "' + queryID + '",' +
+          '"positions": [' + position + ']' +
+        '}' +
+      ']' +
+    '}'
+
+  Http.send(body)
+}
