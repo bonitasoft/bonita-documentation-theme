a41c851300fe3b707ec0dd612433578ef470ac75
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-10-28 17:05:39 +0200

    simplify the helpers function that generate the "commits history" url

--- src/helpers/edit2var.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = (editUrl, type) => {
-  if (!editUrl || !type) return false
-  if (type === 'history') return editUrl.replace(/\/edit\//, '/commits/')
-  return false
-}
