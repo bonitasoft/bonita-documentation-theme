0038f767353afb1ce0c9c982301d410e6f77c128
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-05-05 11:13:14 +0200

    fix condition

--- src/helpers/isSearchBarDisplayed.js
+++ src/helpers/isSearchBarDisplayed.js
@@ -1,5 +1,5 @@
 'use strict'
 
 module.exports = (site, page) => {
-  return !page.layout === '404' && !page.attributes?.['hide-search-bar'] && (site.keys?.forceDisplaySearchBar || !site.keys?.nonProduction)
+  return page.layout !== '404' && !page.attributes?.['hide-search-bar'] && (site.keys?.forceDisplaySearchBar || !site.keys?.nonProduction)
 }
