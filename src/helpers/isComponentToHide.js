7ae6be8dd4fed3d461e1116095e24b4fe0350c52
benjaminparisel <benjamin.parisel@bonitasoft.com>
2024-04-02 16:34:37 +0200

    defect: Build outside preview is broken
    * site.component is an object on the antora 3 side but an array on this preview-src side

--- src/helpers/isComponentToHide.js
+++ src/helpers/isComponentToHide.js
@@ -4,11 +4,22 @@ module.exports = (components, page) => {
   const componentsToHide = page.attributes?.['hide-components']
   if (page.attributes?.['hide-components']) {
     const arrayToHide = trimItems(componentsToHide.split(','))
-    return components.filter((component) => !arrayToHide.includes(component.name))
+    const compos = transformAsAnArrayIfNeeded(components)
+    return compos.filter((component) => !arrayToHide.includes(component.name))
   }
   return components
 
   function trimItems (componentsToHide) {
     return componentsToHide.map((c) => c.trim())
+  }
+
+  /**
+   * Transform the components an array if needed
+   * @param components, could be an array of components or
+   * an object { component1 : {name: component1}, {name:component2, {...}}}
+   * @returns an array of component
+   */
+  function transformAsAnArrayIfNeeded (components) {
+    return Array.isArray(components) ? components : Object.values(components)
   }
 }
