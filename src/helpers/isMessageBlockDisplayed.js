29ff9c7b273f940531707db7ecf1811711bcb68f
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-08-14 16:38:23 +0200

    fix: remove extra top margin in the toc when there is no message block
    Only apply an extra top margin when the HTML generation that a message block is used.

---
+++ src/helpers/isMessageBlockDisplayed.js
@@ -0,0 +1,9 @@
+'use strict'
+
+
+module.exports = (page) => {
+  // To keep in sync with the message blocks that are supported (see partials/toolbar.hbs)
+  return page.attributes?.['next-release'] ||
+    page.attributes?.['custom-message'] ||
+    page.attributes?.['out-of-support']
+}
