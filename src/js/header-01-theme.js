7514353257fc756b819d22f32061bebfb65ab545
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-02-05 13:53:00 +0100

    javascript: remove console.log

--- src/js/header-01-theme.js
+++ src/js/header-01-theme.js
@@ -46,31 +46,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const themeSwitcher = document.getElementById('theme-switcher')
   themeSwitcher.addEventListener('click', (_event) => {
-    console.log('@@click on theme selector')
-    console.log('localStorage:', localStorage)
     const currentTheme = isUsingSystemPreferences() ? 'system' : isDarkTheme() ? 'dark' : 'light'
-    console.log('computed current theme:', currentTheme)
 
     const newTheme = themeOrder.indexOf(currentTheme) === themeOrder.length - 1
       ?  themeOrder[0]
       :  themeOrder[themeOrder.indexOf(currentTheme) + 1]
-    console.log('New theme:', newTheme)
 
     // 3 states are managed with a single variable. When theme is undefined, it means that the theme is managed by system preferences
     if(newTheme === 'system') {
-      console.log('@@device theme')
       localStorage.removeItem('theme')
-      console.log('@@device theme - localStorage:', localStorage)
     } else {
-      console.log('@@non device theme')
       localStorage.setItem('theme', newTheme)
     }
-    console.log('localStorage:', localStorage)
-
     updateTheme()
   })
-
-  console.log('@@INIT localStorage:', localStorage)
 
   // Required to ensure that the right theme for the 'highlight.js' library is used when the page loads
   updateTheme()
